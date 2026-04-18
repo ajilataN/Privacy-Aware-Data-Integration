fa638e7d535f828db99707be43d0749dfebd1120
ajilataN <natali.tashkova@gmail.com>
2026-04-18 18:45:30 +0200

    Refactorin

--- src/configurable/__init__.py
+++ src/configurable/__init__.py
@@ -1,7 +1,7 @@
 from src.configurable.bootstrap import build_bootstrap_config
+from src.configurable.configurator import run_configured_pipeline
 from src.configurable.io_utils import ensure_parent_dir, load_json, write_json
 from src.configurable.models import PipelineOutputs
-from src.configurable.orchestrator import run_configured_pipeline
 
 __all__ = [
     "PipelineOutputs",
